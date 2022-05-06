6e8dd04a25d22040d835d6700f48deb812f59643
KWANHOON LEE <back7930@gmail.com>
2022-05-06 09:09:06 +0900

    feat: portfolio

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
 import './Portfolio.css'
+import IMG1 from '../../assets/frog.gif'
+import IMG2 from '../../assets/musit.gif'
+import IMG3 from '../../assets/naive.png'
+import IMG4 from '../../assets/vote.png'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+         <div className='portfolio__item-image'>
+           <img src={IMG1} alt="" />
+         </div>
+         <h3>Coin Asset Management & Information Provision Website</h3>
+         <div className="portfolio__item-cta">
+         <a href="https://github.com/imysh578/FrogTeam" className='btn'>Github</a>
+         <a href=" " className='btn btn-primary'>livedemo</a>
+         </div>
+        </article>
+        <article className='portfolio__item'>
+         <div className='portfolio__item-image'>
+           <img src={IMG2} alt="" />
+         </div>
+         <h3>Musit X Millim:X ETH NFT Platform</h3>
+         <div className="portfolio__item-cta">
+         <a href="https://github.com/webwebweb3/musit_nft" className='btn'>Github</a>
+         <a href="https://www.notion.so/aa58a42722a54bc9ad5cc70ddaf86ed0" className='btn btn-primary'>Notion</a>
+         </div>
+        </article>
+        <article className='portfolio__item'>
+         <div className='portfolio__item-image'>
+           <img src={IMG3} alt="" />
+         </div>
+         <h3>WebSocket and PoW Website: Training for understanding Blockchain Mining and Transaction process</h3>
+         <div className="portfolio__item-cta">
+         <a href="https://github.com/Flavordash/blockchainPJ_2201" className='btn'>Github</a>
+         <a href="" className='btn btn-primary'>Live Demo</a>
+         </div>
+        </article>
+        <article className='portfolio__item'>
+         <div className='portfolio__item-image'>
+           <img src={IMG4} alt="" />
+         </div>
+         <h3>Solidity Voting App</h3>
+         <div className="portfolio__item-cta">
+         <a href="https://github.com/webwebweb3/voting" className='btn'>Github</a>
+         <a href="https://www.notion.so/Voting-Page-e854a72829b0485191e0433d070f8802"className='btn btn-primary'>Notion</a>
+         </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,9 +1,118 @@
 import React from 'react'
 import './Services.css'
+import {AiOutlineCheckCircle} from 'react-icons/ai'
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className='container services__container'>
+        <article className='service'>
+        <div className='service__head'>
+        <h3>UI/UX Design</h3>
+        </div>
+        
+        <ul className='service__list'>
+          <li>
+            <AiOutlineCheckCircle className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero necessitatibus inventore minus doloribus, consectetur tempora nam similique dolor repudiandae saepe perspiciatis in expedita numquam eum sit vitae explicabo quidem?</p>
+          </li>
+          <li>
+            <AiOutlineCheckCircle className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero necessitatibus inventore minus doloribus, consectetur tempora nam similique dolor repudiandae saepe perspiciatis in expedita numquam eum sit vitae explicabo quidem?</p>
+          </li>
+          <li>
+            <AiOutlineCheckCircle className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero necessitatibus inventore minus doloribus, consectetur tempora nam similique dolor repudiandae saepe perspiciatis in expedita numquam eum sit vitae explicabo quidem?</p>
+          </li>
+          <li>
+            <AiOutlineCheckCircle className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero necessitatibus inventore minus doloribus, consectetur tempora nam similique dolor repudiandae saepe perspiciatis in expedita numquam eum sit vitae explicabo quidem?</p>
+          </li>
+          <li>
+            <AiOutlineCheckCircle className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero necessitatibus inventore minus doloribus, consectetur tempora nam similique dolor repudiandae saepe perspiciatis in expedita numquam eum sit vitae explicabo quidem?</p>
+          </li>
+          <li>
+            <AiOutlineCheckCircle className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero necessitatibus inventore minus doloribus, consectetur tempora nam similique dolor repudiandae saepe perspiciatis in expedita numquam eum sit vitae explicabo quidem?</p>
+          </li>
+          
+        </ul>
+        </article>
+        {/* UX/UI finish */}
+        <article className='service'>
+        <div className='service__head'>
+        <h3>Blockchain Development</h3>
+        </div>
+        
+        <ul className='service__list'>
+          <li>
+            <AiOutlineCheckCircle className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero necessitatibus inventore minus doloribus, consectetur tempora nam similique dolor repudiandae saepe perspiciatis in expedita numquam eum sit vitae explicabo quidem?</p>
+          </li>
+          <li>
+            <AiOutlineCheckCircle className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero necessitatibus inventore minus doloribus, consectetur tempora nam similique dolor repudiandae saepe perspiciatis in expedita numquam eum sit vitae explicabo quidem?</p>
+          </li>
+          <li>
+            <AiOutlineCheckCircle className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero necessitatibus inventore minus doloribus, consectetur tempora nam similique dolor repudiandae saepe perspiciatis in expedita numquam eum sit vitae explicabo quidem?</p>
+          </li>
+          <li>
+            <AiOutlineCheckCircle className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero necessitatibus inventore minus doloribus, consectetur tempora nam similique dolor repudiandae saepe perspiciatis in expedita numquam eum sit vitae explicabo quidem?</p>
+          </li>
+          <li>
+            <AiOutlineCheckCircle className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero necessitatibus inventore minus doloribus, consectetur tempora nam similique dolor repudiandae saepe perspiciatis in expedita numquam eum sit vitae explicabo quidem?</p>
+          </li>
+          <li>
+            <AiOutlineCheckCircle className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero necessitatibus inventore minus doloribus, consectetur tempora nam similique dolor repudiandae saepe perspiciatis in expedita numquam eum sit vitae explicabo quidem?</p>
+          </li>
+          
+        </ul>
+        </article>
+        {/* UX/UI finish */}
+        <article className='service'>
+        <div className='service__head'>
+        <h3>Content Creation</h3>
+        </div>
+        
+        <ul className='service__list'>
+          <li>
+            <AiOutlineCheckCircle className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero necessitatibus inventore minus doloribus, consectetur tempora nam similique dolor repudiandae saepe perspiciatis in expedita numquam eum sit vitae explicabo quidem?</p>
+          </li>
+          <li>
+            <AiOutlineCheckCircle className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero necessitatibus inventore minus doloribus, consectetur tempora nam similique dolor repudiandae saepe perspiciatis in expedita numquam eum sit vitae explicabo quidem?</p>
+          </li>
+          <li>
+            <AiOutlineCheckCircle className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero necessitatibus inventore minus doloribus, consectetur tempora nam similique dolor repudiandae saepe perspiciatis in expedita numquam eum sit vitae explicabo quidem?</p>
+          </li>
+          <li>
+            <AiOutlineCheckCircle className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero necessitatibus inventore minus doloribus, consectetur tempora nam similique dolor repudiandae saepe perspiciatis in expedita numquam eum sit vitae explicabo quidem?</p>
+          </li>
+          <li>
+            <AiOutlineCheckCircle className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero necessitatibus inventore minus doloribus, consectetur tempora nam similique dolor repudiandae saepe perspiciatis in expedita numquam eum sit vitae explicabo quidem?</p>
+          </li>
+          <li>
+            <AiOutlineCheckCircle className='service__list-icon' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis libero necessitatibus inventore minus doloribus, consectetur tempora nam similique dolor repudiandae saepe perspiciatis in expedita numquam eum sit vitae explicabo quidem?</p>
+          </li>
+          
+        </ul>
+        </article>
+        {/* UX/UI finish */}
+      </div>
+    </section>
   )
 }
 
